86a466b48742bc1fc6d0df43a6b7d710fa83418b
Firsov Roman <RGFirsov@alfabank.ru>
2021-09-22 22:06:00 +0300

    feat(client): add layout

--- src/containers/app/app.tsx
+++ src/containers/app/app.tsx
@@ -1,3 +1,12 @@
-const App = () => <div>App</div>
+import React from "react";
+import { cn as createCn } from "@bem-react/classname";
+
+import Layout from "../../components/layout";
+
+import "./app.css";
+
+const cn = createCn("app");
+
+const App = () => <div className={ cn() }><Layout /></div>
 
 export default App;
